f556de7f5825961829be9eb315d508e7b060dfc0
Trais McAllister <tbm0115@gmail.com>
2023-02-21 15:55:59 -0800

    Sub-Type Validation (#25)
    * Added ObservationalSubType
    
     - Added attribute `ObservationalSubType` to indicate when a DataItem type has a subType
     - Added validation of the SubType property on a DataItem when the relevant type in the enum has been decorated with `ObservationalSubType`.
     - Added scriban template to render the `ObservationalSubType` attribute
     - Updated the enum scriban template to include the `ObservationalSubType` template.
     - Updated the transpiler to include references to subTypes on the CATEGORY enum.
    
    * Implemented scriban template
    
     - Updated scriban template for enum to produce `ObservationalSubType` attributes on relevant DataItem type enum fields.
     - Ran the transpiler to go ahead and perform the update.

--- MtconnectTranspiler.Sinks.MtconnectCore/Models/MtconnectCoreEnum.cs
+++ MtconnectTranspiler.Sinks.MtconnectCore/Models/MtconnectCoreEnum.cs
@@ -8,6 +8,9 @@ namespace MtconnectTranspiler.Sinks.MtconnectCore.Models
     [ScribanTemplate("MtconnectCore.Enum.scriban")]
     public class MtconnectCoreEnum : CSharp.Models.Enum
     {
+        // NOTE: Only used for CATEGORY types that have subTypes.
+        public Dictionary<string, string> SubTypes { get; set; } = new Dictionary<string, string>();
+
         public MtconnectCoreEnum(MTConnectModel model, XmiElement source, string name) : base(model, source, name) { }
 
         public MtconnectCoreEnum(MTConnectModel model, UmlEnumeration source) : base(model, source) { }
